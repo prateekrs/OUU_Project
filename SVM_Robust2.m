4c2972b911a35f348eb34cc5d45f3860499b04bc
Areesh <areesh0612@gmail.com>
2017-11-27 21:33:08 -0600

    robust2

---
+++ SVM_Robust2.m
@@ -0,0 +1,44 @@
+function [w,w0] = SVM_Robust(points,labels,budget)
+options = sdpsettings('verbose', 0, 'solver', 'mosek');
+
+[N,dims] = size(points);
+
+e = ones(N,1);
+S = [eye(N), -eye(N), eye(N), zeros(N,1);
+     zeros(1,N), e', e', 1 ];
+t = [labels;budget];
+
+q = e;
+K = length(t);
+L = length(q);
+
+tc = [q;t];
+
+%-----VARIABLES-----
+w = sdpvar(dims,1);
+w0 = sdpvar();
+T = [-2*diag(points*w - w0), zeros(N,2*N+1);
+     zeros(N,3*N+1)];
+W = [eye(N);eye(N)];
+h = [1+ points*w - w0;zeros(N,1)];
+
+[I,J] = size(T);
+Tc = [zeros(J) T'/2;T/2 zeros(I)];%size:I+J
+Sc = [zeros(L,J) W';S zeros(K,I)];
+
+x = sdpvar(N,1);
+hc = [zeros(J,1); h];
+psi = sdpvar(L+K,1);
+phi = sdpvar(L+K,1);
+tau = sdpvar;
+M = [Sc'*diag(phi)*Sc-Tc, .5*(Sc'*psi-hc);.5*(Sc'*psi-hc)' tau];
+PP = sdpvar(length(M));
+NN = sdpvar(length(M));
+%------
+cons = [M==PP+NN;PP>=0;NN(:)>=0];
+
+obj = tc'*psi + (tc.^2)'*phi + tau;
+out = optimize(cons,obj,options);
+w = double(w);
+w0 = double(w0);
+end
